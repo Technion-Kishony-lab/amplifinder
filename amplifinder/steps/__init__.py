cee96516302e06abb0add149ff844e70e2c552e0
rkishony <rkishony@technion.ac.il>
2025-12-06 00:23:02 +0200

    add IS finding by GenBank annotations

--- amplifinder/steps/__init__.py
+++ amplifinder/steps/__init__.py
@@ -3,9 +3,11 @@
 from amplifinder.steps.base import Step
 from amplifinder.steps.initialize import InitializingStep
 from amplifinder.steps.get_reference import GetReferenceStep
+from amplifinder.steps.locate_iss_genbank import LocateISsUsingGenbank
 
 __all__ = [
     "Step",
     "InitializingStep",
     "GetReferenceStep",
+    "LocateISsUsingGenbank",
 ]
