1831447e5f9be9f9976a2b2257d3d354b4b0175e
rkishony <rkishony@technion.ac.il>
2025-12-06 10:17:22 +0200

    refactor: move breseq fields from CSV to Python module

---
+++ amplifinder/data/breseq_fields/__init__.py
@@ -0,0 +1,17 @@
+"""Breseq record type field definitions."""
+
+from pathlib import Path
+from typing import Dict
+
+from amplifinder.data_types.schema import TypedSchema
+
+RECORD_TYPES = ["JC", "SNP", "MOB", "DEL", "UN"]
+
+_FIELDS_PATH = Path(__file__).parent
+
+
+def load_all_field_defs() -> Dict[str, TypedSchema]:
+    """Load all field definitions."""
+    return {name: TypedSchema.from_csv(_FIELDS_PATH / f"{name}_fields.csv") 
+            for name in RECORD_TYPES}
+
