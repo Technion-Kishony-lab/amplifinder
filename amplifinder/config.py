474ed64a499c61dbcb1fb9472d705d9395d4edcc
rkishony <rkishony@technion.ac.il>
2026-01-18 14:14:55 +0200

    neg_threshold_rel for calling junctions

--- amplifinder/config.py
+++ amplifinder/config.py
@@ -39,8 +39,12 @@ class BowtieParams(FrozenParams):
 class JcCallParams(FrozenParams):
     """Parameters for junction coverage calling (exists, not exists, ambiguous)."""
 
-    min_jc_cov: int = 5
-    num_std: int = 3
+    # for a jct to be negative, the number of spanning reads should be less than:
+    neg_threshold_abs: int = 5  # absolute number of spanning reads, or
+    neg_threshold_rel: float = 0.01  # fraction of the expected number of spanning reads
+
+    # for a jct to be positive, the number of spanning reads should be greater than the expected number minus x standard deviations
+    pos_threshold_in_num_std_below_expected: int = 3
 
 
 # Default configuration values (from config.txt and added alignment params)
